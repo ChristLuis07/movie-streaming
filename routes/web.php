7ffb23033dcc651fc92b64cc452ea1e9023c58f0
Christian <christianluisginting@gmail.com>
2025-03-28 10:05:51 +0700

    feat: slicing sign in and sign up

--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,8 @@ Route::get('/', function () {
     ]);
 });
 
+Route::redirect('/', '/prototype/login');
+
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
@@ -35,6 +37,15 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::prefix('prototype')->name('prototype.')->group(function () {
+    route::get('/login', function () {
+        return Inertia::render('Prototype/Login');
+    })->name('login');
+    route::get('/register', function () {
+        return Inertia::render('Prototype/Register');
+    })->name('register');
 });
 
 require __DIR__.'/auth.php';
