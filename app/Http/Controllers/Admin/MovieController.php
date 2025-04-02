8dc53b0b555bfa65120445257e0106eaec4858b4
Christian <christianluisginting@gmail.com>
2025-04-02 13:07:45 +0700

    feat: delete and restore

--- app/Http/Controllers/Admin/MovieController.php
+++ app/Http/Controllers/Admin/MovieController.php
@@ -20,7 +20,7 @@ class MovieController extends Controller
      */
     public function index()
     {
-        $movies = Movie::all();
+        $movies = Movie::withTrashed()->orderBy('deleted_at')->get();
         return Inertia::render('Admin/Movie/Index', [
             'movies' => $movies,
         ]);
@@ -112,6 +112,18 @@ public function create()
      */
     public function destroy(Movie $movie)
     {
-        //
+        $movie->delete();
+        return redirect(route('admin.dashboard.movie.index'))->with([
+            'message' => 'Movie Deleted Successfully',
+            'type' => 'success',
+        ]);
+    }
+
+    public function restore($movie) {
+        Movie::withTrashed()->find($movie)->restore();
+        return redirect(route('admin.dashboard.movie.index'))->with([
+            'message' => 'Movie Restored Successfully',
+            'type' => 'success',
+        ]);
     }
 }
